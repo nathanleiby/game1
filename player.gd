ed17a2cfe288a96e481069a229d214f522014d7d
Nathan Leiby <nathanleiby@gmail.com>
2023-10-20 13:48:56 -0700

    code cleanup, also tweak player collision box

--- player.gd
+++ player.gd
@@ -21,12 +21,27 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+	
+	# Change XZ direction
+	if direction != Vector3.ZERO:
+		$Pivot.look_at(position + direction, Vector3.UP)
+		
+	# Normalize direction vector, so that diagonal movement isn't faster
+	direction = direction.normalized()
+	
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Vertical Velocity
+	if not is_on_floor(): # if in air, fall. gravity!
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 		
 	# Jumping
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		target_velocity.y = jump_impulse
 		
-	# collisions
+	# Collisions
 	for index in range(get_slide_collision_count()):
 		var collision = get_slide_collision(index)
 		
@@ -37,23 +52,10 @@ func _physics_process(delta):
 		if collision.get_collider().is_in_group("mob"):
 			var mob = collision.get_collider()
 			# check that we are colliding from above
-			# print("Collision normal:", collision.get_normal(), " ---> UP dot product = ", Vector3.UP.dot(collision.get_normal()))
 			if Vector3.UP.dot(collision.get_normal()) > 0.75: # TODO: was 0.1 in example
 				mob.squash()
 				target_velocity.y = bounce_impulse	
 			
-		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.look_at(position + direction, Vector3.UP)
-
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-	
-	# Vertical Velocity
-	if not is_on_floor(): # if in air, fall. gravity!
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
 	# Moving
 	velocity = target_velocity
