fd4d43406b399b13b6eb76b2f4b933e5b001e76a
Nathan Leiby <nathanleiby@gmail.com>
2023-10-19 22:48:12 -0700

    basic monster aka 'mob'
    https://docs.godotengine.org/en/stable/getting_started/first_3d_game/04.mob_scene.html

---
+++ Mob.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+@export var min_speed = 10
+@export var max_speed = 18
+
+func _physics_process(delta):
+	move_and_slide()
+	
+func initialize(start_pos, player_pos):
+	# random speed
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	
+	# adjust velocity toward player, with some randomness
+	look_at_from_position(start_pos, player_pos, Vector3.UP)
+	rotate_y(randf_range(-PI/4, PI/4))
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+
