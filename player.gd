b917f3a1edaf06259028b83263a2ca5c4395acea
Nathan Leiby <nathanleiby@gmail.com>
2023-10-20 11:50:29 -0700

    add player jumping

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
 
@@ -17,7 +18,10 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-
+	# Jumping
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
