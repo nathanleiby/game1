27f55b7f88370c4b2c8e382f906915c2e977ba85
Nathan Leiby <nathanleiby@gmail.com>
2023-10-20 14:13:49 -0700

    allow restarting game after death

--- main.gd
+++ main.gd
@@ -4,6 +4,8 @@ extends Node
 @export var mob_scene: PackedScene
 
 
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -21,3 +23,9 @@ func _on_timer_timeout():
 
 func _on_player_hit():	
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# restart the current scene
+		get_tree().reload_current_scene()
